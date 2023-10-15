d30f449575bce20124179344239a70c6348c8538
Tanush <140692804+08Tanush@users.noreply.github.com>
2023-10-15 16:27:19 +0530

    Update 27_21_tanush.c
    new one

---
+++ 27_21_tanush.c
@@ -0,0 +1,54 @@
+/*
+Author Name=Tanush karane
+Date = 6/10/2023
+Branch = I.T.
+Roll No=27
+Program =02
+Description=Program to implement or Selection Sort
+Date = 6/10/23
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void selectionsort(int *a,int n)
+{
+ int i,j,k,temp;
+ for(i=0;i<n;i++)
+	  {
+	  for(j=i+1;j<n;j++)
+	    {
+	     if(a[i]>a[j])
+	       {
+		   temp=a[i];
+		   a[i]=a[j];
+		   a[j]=temp;
+	       }
+	     }
+	     printf("\n pass %d  :",i+1);
+	     for(k=0;k<n;k++)
+	       printf("%d ",a[k]);
+  }
+}
+
+
+int main()
+{
+     int n,i,j,temp,a[20];
+
+ // clrscr();
+  printf("Enter total elements: ");
+  scanf("%d",&n);
+
+  printf("Enter %d elements: ",n);
+  for(i=0;i<n;i++)
+      scanf("%d",&a[i]);
+
+  selectionsort(a,n);
+
+  printf("\nAfter sorting is: ");
+  for(i=0;i<n;i++)
+      printf(" %d",a[i]);
+
+//  getch();
+    return 0;
+}
